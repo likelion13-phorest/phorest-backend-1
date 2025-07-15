47ceaac9c58c5fe30e411a91d241039adaf2f238
mongsam2 <songjaehyun121476@gmail.com>
2025-07-15 16:02:31 +0900

    init

---
+++ src/main/java/likelion13/phorest/PhorestApplication.java
@@ -0,0 +1,13 @@
+package likelion13.phorest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhorestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhorestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/likelion13/phorest/PhorestApplicationTests.java
@@ -0,0 +1,13 @@
+package likelion13.phorest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhorestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
